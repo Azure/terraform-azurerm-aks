1d12e11a2b793d4f834d98d00ec74c1cf9ab11bb
Steven Black <genseb7@gmail.com>
2019-08-12 17:11:48 -0400

    upgraded module log-analytics-workspace

--- modules/log-analytics-workspace/outputs.tf
+++ modules/log-analytics-workspace/outputs.tf
@@ -1,7 +1,8 @@
 output "id" {
-  value = "${azurerm_log_analytics_workspace.main.id}"
+  value = azurerm_log_analytics_workspace.main.id
 }
 
 output "name" {
-  value = "${azurerm_log_analytics_workspace.main.name}"
+  value = azurerm_log_analytics_workspace.main.name
 }
+
