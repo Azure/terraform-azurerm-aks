cd17b71dc9da8f094223cfcfafea995cbe9debac
Steven B <genseb7@gmail.com>
2019-09-24 16:14:58 -0400

    -check-variables=false not in terraform v12

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ COMMANDS=()
 if [ $1 == "validate" ] || [ $1 == "full" ]
 then
     COMMANDS+=("terraform fmt -check=true")
-    COMMANDS+=("terraform validate -check-variables=false")
+    COMMANDS+=("terraform validate")
     COMMANDS+=("dep ensure")
 fi
 
