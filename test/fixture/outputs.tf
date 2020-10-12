ad8e2bd9c18b67a3a1f04156be2142c7108ba103
Yuping Wei <56525716+yupwei68@users.noreply.github.com>
2020-10-12 17:15:53 +0800

    Update outputs.tf

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -9,7 +9,3 @@ output "test_aks_without_monitor_id" {
 output "test_aks_without_monitor_identity" {
   value = module.aks_without_monitor.system_assigned_identity
 }
-
-output "test_aks_paid_sku_id" {
-  value = module.aks_paid_sku.aks_id
-}
