1e5d514ae519552cef29ada67cfe62a012be12fe
Bruno Amaral <brunoamaralf@gmail.com>
2020-10-30 17:44:23 -0300

    Add default_node_pool var example

--- variables_default_nodepool.tf
+++ variables_default_nodepool.tf
@@ -36,6 +36,24 @@ variable "default_node_pool" {
       max_count             = null
       min_count             = null
     }]
-    # Example:
-    
+##    Example:
+#
+#     default = [{
+#       orchestrator_version  = null
+#       name                  = default
+#       node_count            = 3
+#       vm_size               = "Standard_D2s_v3"
+#       os_disk_size_gb       = 50
+#       vnet_subnet_id        = null
+#       enable_node_public_ip = null
+#       enable_auto_scaling   = true
+#       availability_zones    = null
+#       node_labels           = null
+#       node_taints           = null
+#       type                  = null
+#       tags                  = null
+#       max_pods              = null
+#       max_count             = 10
+#       min_count             = 2
+# }]
 }
