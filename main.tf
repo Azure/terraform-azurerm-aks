88fdd3da8a6d138122783ec63b61d5ce8d24d886
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2020-11-08 09:32:13 +0100

    Add support for private cluster API access.

--- main.tf
+++ main.tf
@@ -14,6 +14,8 @@ resource "azurerm_kubernetes_cluster" "main" {
   dns_prefix          = var.prefix
   sku_tier            = var.sku_tier
 
+  private_cluster_enabled = var.private_cluster_enabled
+
   linux_profile {
     admin_username = var.admin_username
 
