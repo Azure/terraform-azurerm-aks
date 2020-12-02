da615f00c784644f6c914f5a034150ecaf52fa3d
Yuping Wei <56525716+yupwei68@users.noreply.github.com>
2020-12-02 16:20:57 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "log_retention_in_days" {
 variable "agents_count" {
   description = "The number of Agents that should exist in the Agent Pool"
   type        = number
-  default     = 2
+  default     = null
 }
 
 variable "public_ssh_key" {
