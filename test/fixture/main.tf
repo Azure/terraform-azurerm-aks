e17f56732a70826a6a70f591a288d7ab345cc26e
Yuping Wei <56525716+yupwei68@users.noreply.github.com>
2020-12-16 11:19:02 +0800

    Update main.tf
    integrate the enhancement fields into module "aks"

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -38,12 +38,28 @@ module "aks" {
   enable_http_application_routing = true
   enable_azure_policy             = true
   sku_tier                        = "Paid"
-  enable_kube_dashboard           = true
   private_cluster_enabled         = true
   enable_auto_scaling             = true
   agents_min_count                = 1
   agents_max_count                = 2
   agents_count                    = null
+  agents_max_pods                 = 100
+  agents_pool_name                = "testnodepool"
+  agents_availability_zones       = ["1", "2"]
+  agents_type                     = "VirtualMachineScaleSets"
+
+  agents_labels = {
+    "node1" : "label1"
+  }
+
+  agents_tags = {
+    "Agent" : "agentTag"
+  }
+
+  network_policy                 = "azure"
+  net_profile_dns_service_ip     = "10.0.0.10"
+  net_profile_docker_bridge_cidr = "170.10.0.1/16"
+  net_profile_service_cidr       = "10.0.0.0/16"
 
   depends_on = [azurerm_resource_group.main]
 }
@@ -53,36 +69,7 @@ module "aks_without_monitor" {
   prefix                         = "prefix2-${random_id.prefix.hex}"
   resource_group_name            = azurerm_resource_group.main.name
   enable_log_analytics_workspace = false
+  enable_kube_dashboard          = false
+  net_profile_pod_cidr           = "10.1.0.0/16"
   depends_on                     = [azurerm_resource_group.main]
-}
-
-module "aks_network_profile_config" {
-  source                          = "../.."
-  prefix                          = "prefix-${random_id.prefix.hex}"
-  resource_group_name             = azurerm_resource_group.main.name
-  client_id                       = var.client_id
-  client_secret                   = var.client_secret
-  kubernetes_version              = "1.19.3"
-  orchestrator_version            = "1.19.3"
-  network_plugin                  = "azure"
-  vnet_subnet_id                  = azurerm_subnet.test.id
-  os_disk_size_gb                 = 60
-  enable_http_application_routing = true
-  enable_azure_policy             = true
-  sku_tier                        = "Paid"
-  enable_kube_dashboard           = true
-  private_cluster_enabled         = true
-  enable_node_public_ip           = false
-  enable_auto_scaling             = true
-  agents_min_count                = 1
-  agents_max_count                = 2
-  agents_max_pods                 = 100
-  network_plugin                  = "azure"
-  network_policy                  = "azure"
-  net_profile_docker_bridge_cidr  = "170.10.0.1/16"
-  net_profile_service_cidr        = "10.0.0.0/16"
-  net_profile_dns_service_ip      = "10.0.0.10"
-
-  
-  depends_on = [azurerm_resource_group.main]
 }
