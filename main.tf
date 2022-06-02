e8bd9ba030e4b53cd9d18a45f25af8f08d6810b6
David J. M. Karlsen <david@davidkarlsen.com>
2022-06-02 15:24:33 +0200

    do on workspace too

--- main.tf
+++ main.tf
@@ -156,7 +156,7 @@ resource "azurerm_kubernetes_cluster" "main" {
 resource "azurerm_log_analytics_workspace" "main" {
   count               = var.enable_log_analytics_workspace ? 1 : 0
   name                = var.cluster_log_analytics_workspace_name == null ? "${var.prefix}-workspace" : var.cluster_log_analytics_workspace_name
-  location            = data.azurerm_resource_group.main.location
+  location            = coalesce(var.location, data.azurerm_resource_group.main.location)
   resource_group_name = var.resource_group_name
   sku                 = var.log_analytics_workspace_sku
   retention_in_days   = var.log_retention_in_days
