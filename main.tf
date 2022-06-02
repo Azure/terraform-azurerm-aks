e5a81200a3b69cbe0b851e91f6673c8a2a30906c
David J. M. Karlsen <david@davidkarlsen.com>
2022-06-02 15:21:06 +0200

    do same on log analytics for consistency

--- main.tf
+++ main.tf
@@ -167,7 +167,7 @@ resource "azurerm_log_analytics_workspace" "main" {
 resource "azurerm_log_analytics_solution" "main" {
   count                 = var.enable_log_analytics_workspace ? 1 : 0
   solution_name         = "ContainerInsights"
-  location              = data.azurerm_resource_group.main.location
+  location              = coalesce(var.location, data.azurerm_resource_group.main.location)
   resource_group_name   = var.resource_group_name
   workspace_resource_id = azurerm_log_analytics_workspace.main[0].id
   workspace_name        = azurerm_log_analytics_workspace.main[0].name
