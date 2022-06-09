6af997774d167e08f43d49144461b20ba6cdd1cc
Facundo Gauna <facundo@boxboat.com>
2022-06-09 10:46:45 -0400

    Added private dns zone id as a variable

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   dns_prefix              = var.prefix
   sku_tier                = var.sku_tier
   private_cluster_enabled = var.private_cluster_enabled
+  private_dns_zone_id     = var.private_dns_zone_id
 
   linux_profile {
     admin_username = var.admin_username
