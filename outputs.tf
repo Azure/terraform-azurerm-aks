363c097496bd9776bc1b4f9bea976d4b874a9aee
Toni <toni.tauro@adfinis.com>
2022-06-15 13:57:03 +0100

    fix(outputs.tf) sensitive values

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,32 @@
+
 output "client_key" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+  sensitive = true
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+  sensitive = true
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].host
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].host
+  sensitive = true
 }
 
 output "username" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].username
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].username
+  sensitive = true
 }
 
 output "password" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].password
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].password
+  sensitive = true
 }
 
 output "node_resource_group" {
