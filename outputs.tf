c6c308cd85bd72d2a7fdbdf6d390317dfa949eff
Toni Tauro <toni.tauro@adfinis.com>
2022-06-16 09:58:06 +0200

    fix(ouptuts): sensitive tests
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,32 @@
 
 output "client_key" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_key
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_key
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
 }
 
 output "host" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].host
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].host
 }
 
 output "username" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].username
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].username
 }
 
 output "password" {
-  value     = azurerm_kubernetes_cluster.main.kube_config[0].password
   sensitive = true
+  value     = azurerm_kubernetes_cluster.main.kube_config[0].password
 }
 
 output "node_resource_group" {
