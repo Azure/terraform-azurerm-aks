c6a1c687065df14c1d5f117558566f3c2d412e4b
zjhe <hezijie@microsoft.com>
2022-06-23 10:42:20 +0800

    Add arguments to `azure_active_directory_role_based_access_control` block, turn on rbac for test code.

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -11,27 +11,33 @@ output "test_aks_without_monitor_identity" {
 }
 
 output "test_admin_client_key" {
-  value = module.aks.admin_client_key
+  sensitive = true
+  value     = module.aks.admin_client_key
 }
 
 output "test_admin_client_certificate" {
-  value = module.aks.admin_client_certificate
+  sensitive = true
+  value     = module.aks.admin_client_certificate
 }
 
 output "test_admin_cluster_ca_certificate" {
-  value = module.aks.admin_client_certificate
+  sensitive = true
+  value     = module.aks.admin_client_certificate
 }
 
 output "test_admin_host" {
-  value = module.aks.admin_host
+  sensitive = true
+  value     = module.aks.admin_host
 }
 
 output "test_admin_username" {
-  value = module.aks.admin_username
+  sensitive = true
+  value     = module.aks.admin_username
 }
 
 output "test_admin_password" {
-  value = module.aks.admin_password
+  sensitive = true
+  value     = module.aks.admin_password
 }
 
 output "test_client_key" {
