48163d81a92af8ffa4113a15440ccf1a1812c168
lukasz.szczesniak <lukasz.r.szczesniak@gmail.com>
2022-07-07 20:11:12 +0200

    feat: Support existing log analytics workspace in different RG (#211)

--- main.tf
+++ main.tf
@@ -167,28 +167,12 @@ resource "azurerm_log_analytics_workspace" "main" {
 }
 
 resource "azurerm_log_analytics_solution" "main" {
-  count                 = var.enable_log_analytics_workspace && var.log_analytics_workspace == null ? 1 : 0
+  count                 = var.enable_log_analytics_workspace ? 1 : 0
   solution_name         = "ContainerInsights"
   location              = coalesce(var.location, data.azurerm_resource_group.main.location)
-  resource_group_name   = var.resource_group_name
-  workspace_resource_id = azurerm_log_analytics_workspace.main[0].id
-  workspace_name        = azurerm_log_analytics_workspace.main[0].name
-
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_log_analytics_solution" "main" {
-  count                 = var.enable_log_analytics_workspace && var.log_analytics_workspace != null ? 1 : 0
-  solution_name         = "ContainerInsights"
-  location              = coalesce(var.location, data.azurerm_resource_group.main.location)
-  resource_group_name   = var.log_analytics_workspace.resource_group_name != null ? var.log_analytics_workspace.resource_group_name : var.resource_group_name
-  workspace_resource_id = var.log_analytics_workspace.id
-  workspace_name        = var.log_analytics_workspace.name
+  resource_group_name   = var.log_analytics_workspace != null && var.log_analytics_workspace.resource_group_name != null ? var.log_analytics_workspace.resource_group_name : var.resource_group_name
+  workspace_resource_id = var.log_analytics_workspace != null ? var.log_analytics_workspace.id : azurerm_log_analytics_workspace.main[0].id
+  workspace_name        = var.log_analytics_workspace != null ? var.log_analytics_workspace.name : azurerm_log_analytics_workspace.main[0].name
 
   plan {
     publisher = "Microsoft"
