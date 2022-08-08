442e414da226d68bf5623323955b60c5cf915b88
zjhe <hezijie@microsoft.com>
2022-08-08 21:57:30 +0800

    Remove default value for the `key_vault_firewall_bypass_ip_cidr` so the user won't miss it.

--- test/fixture/variables.tf
+++ test/fixture/variables.tf
@@ -3,8 +3,8 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "key_vault_firewall_bypass_ip_cidr" {
-  type    = string
-  default = null
+  type        = string
+  description = "This Terraform script will provision a new Azure KeyVault key so this machine's public ip should be put into KeyVault's firewall allow list."
 }
 
 variable "location" {
