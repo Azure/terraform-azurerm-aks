7e469d7fd5abfe6f7ead5dae50ca9c7f682d7d81
zjhe <hezijie@microsoft.com>
2022-09-05 16:02:45 +0800

    Add version upgrade test

---
+++ test/upgrade/upgrade_test.go
@@ -0,0 +1,76 @@
+package upgrade
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_helper "github.com/lonegunmanb/terraform-module-test-helper"
+)
+
+func TestExampleUpgrade_startup(t *testing.T) {
+	currentRoot, err := test_helper.GetCurrentModuleRootPath()
+	if err != nil {
+		t.FailNow()
+	}
+	currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
+	if err != nil {
+		t.FailNow()
+	}
+	vars := map[string]interface{}{
+		"client_id":     "",
+		"client_secret": "",
+	}
+	managedIdentityId := os.Getenv("MSI_ID")
+	if managedIdentityId != "" {
+		vars["managed_identity_principal_id"] = managedIdentityId
+	}
+	test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-aks", "examples/startup", currentRoot, terraform.Options{
+		Upgrade: true,
+		Vars:    vars,
+	}, currentMajorVersion)
+}
+
+func TestExampleUpgrade_without_monitor(t *testing.T) {
+	currentRoot, err := test_helper.GetCurrentModuleRootPath()
+	if err != nil {
+		t.FailNow()
+	}
+	currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
+	if err != nil {
+		t.FailNow()
+	}
+	var vars map[string]interface{}
+	managedIdentityId := os.Getenv("MSI_ID")
+	if managedIdentityId != "" {
+		vars = map[string]interface{}{
+			"managed_identity_principal_id": managedIdentityId,
+		}
+	}
+	test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-aks", "examples/without_monitor", currentRoot, terraform.Options{
+		Upgrade: true,
+		Vars:    vars,
+	}, currentMajorVersion)
+}
+
+func TestExampleUpgrade_named_cluster(t *testing.T) {
+	currentRoot, err := test_helper.GetCurrentModuleRootPath()
+	if err != nil {
+		t.FailNow()
+	}
+	currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
+	if err != nil {
+		t.FailNow()
+	}
+	var vars map[string]interface{}
+	managedIdentityId := os.Getenv("MSI_ID")
+	if managedIdentityId != "" {
+		vars = map[string]interface{}{
+			"managed_identity_principal_id": managedIdentityId,
+		}
+	}
+	test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-aks", "examples/named_cluster", currentRoot, terraform.Options{
+		Upgrade: true,
+		Vars:    vars,
+	}, currentMajorVersion)
+}
