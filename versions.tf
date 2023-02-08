01fc5c4b930882fff3b778b70b64ac6ba7ab339d
zjhe <hezijie@microsoft.com>
2023-02-08 21:31:26 +0800

    add support for web_app_routing

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.40"
+      version = ">= 3.40, < 4.0"
     }
     tls = {
       source  = "hashicorp/tls"
