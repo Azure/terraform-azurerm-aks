27b416d8b8bbeddd5b3577ebd173ada00e5bea72
Iago Santos Pardo <iago.santos.pardo@adfinis.com>
2023-09-07 09:58:10 +0200

    feat(custom-ca-trust): Enable custom_ca_trust_enabled in node_pools
    Related to: https://github.com/Azure/terraform-azurerm-aks/issues/438
    
    Signed-off-by: Iago Santos Pardo <iago.santos.pardo@adfinis.com>

--- main.tf
+++ main.tf
@@ -52,6 +52,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     content {
       name                         = var.agents_pool_name
       vm_size                      = var.agents_size
+      custom_ca_trust_enabled      = var.custom_ca_trust_enabled
       enable_auto_scaling          = var.enable_auto_scaling
       enable_host_encryption       = var.enable_host_encryption
       enable_node_public_ip        = var.enable_node_public_ip
@@ -152,6 +153,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     content {
       name                         = var.agents_pool_name
       vm_size                      = var.agents_size
+      custom_ca_trust_enabled      = var.custom_ca_trust_enabled
       enable_auto_scaling          = var.enable_auto_scaling
       enable_host_encryption       = var.enable_host_encryption
       enable_node_public_ip        = var.enable_node_public_ip
