498e79acd90b2619cc82d0356f7c32b3ed842401
Casper Nilsen <admin.casper.nilsen@akerbp.com>
2023-09-22 11:34:26 +0200

    remove nullable

--- variables.tf
+++ variables.tf
@@ -693,7 +693,6 @@ variable "msi_auth_for_monitoring_enabled" {
   type        = bool
   default     = null
   description = "(Optional) Is managed identity authentication for monitoring enabled? Defaults to `false`"
-  nullable    = true
 }
 
 variable "microsoft_defender_enabled" {
