62ad20b49a222f78dcbdf6704febd8c987a0febd
zjhe <lonegunmanb@hotmail.com>
2024-09-09 13:14:24 +0800

    with_acr

---
+++ examples/startup_v4/outputs.tf
@@ -0,0 +1,1 @@
+../startup/outputs.tf
---
+++ examples/uai_and_assign_role_on_subnet_v4/providers_override.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
