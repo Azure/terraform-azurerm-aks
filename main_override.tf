08cda8ba8cd7453021585e74cb82d70443716de1
hezijie <lonegunmanb@hotmail.com>
2024-09-09 14:29:18 +0800

    fix format

--- main_override.tf
+++ main_override.tf
@@ -1,4 +1,4 @@
 resource "azurerm_kubernetes_cluster" "main" {
-  automatic_channel_upgrade           = var.automatic_channel_upgrade
-  node_os_channel_upgrade             = var.node_os_channel_upgrade
+  automatic_channel_upgrade = var.automatic_channel_upgrade
+  node_os_channel_upgrade   = var.node_os_channel_upgrade
 }
