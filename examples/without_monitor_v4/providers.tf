655170cdf3213520e9b9f66dafc3b1873277ab7d
hezijie <lonegunmanb@hotmail.com>
2024-09-09 13:31:37 +0800

    without_monitor

---
+++ examples/without_monitor_v4/providers.tf
@@ -0,0 +1,1 @@
+../without_monitor/providers.tf
---
+++ examples/without_monitor_v4/providers_override.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    curl = {
+      source  = "anschoewe/curl"
+      version = "1.0.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
