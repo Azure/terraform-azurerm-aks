e725635f372684b8153374c023a8a3ff5d09252d
Zijie He <hezijie@microsoft.com>
2024-12-05 11:28:55 +0800

    move deprecated variables into deprecated_variables.tf file

---
+++ deprecated_variables.tf
@@ -0,0 +1,29 @@
+variable "agents_taints" {
+  type        = list(string)
+  default     = null
+  description = "DEPRECATED, (Optional) A list of the taints added to new nodes during node pool create and scale. Changing this forces a new resource to be created."
+}
+
+variable "api_server_subnet_id" {
+  type        = string
+  default     = null
+  description = "DEPRECATED, (Optional) The ID of the Subnet where the API server endpoint is delegated to."
+}
+
+variable "rbac_aad_client_app_id" {
+  type        = string
+  default     = null
+  description = "DEPRECATED, The Client ID of an Azure Active Directory Application."
+}
+
+variable "rbac_aad_server_app_id" {
+  type        = string
+  default     = null
+  description = "DEPRECATED, The Server ID of an Azure Active Directory Application."
+}
+
+variable "rbac_aad_server_app_secret" {
+  type        = string
+  default     = null
+  description = "DEPRECATED, The Server Secret of an Azure Active Directory Application."
+}
