685237c833f0495a17204d311151745dd698a23e
Saverio Proto <saverioproto@microsoft.com>
2025-01-15 14:33:08 +0100

    Bump azapi provider to >=2.0, < 3.0

--- v4/versions_override.tf
+++ v4/versions_override.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.4.0, < 2.0"
+      version = ">=2.0, < 3.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
