0c8d9803548b02ea7b9fe95d289568bcdd4cd585
Anson Qian <ansonqian@microsoft.com>
2025-10-17 23:48:57 -0400

    fix

--- examples/localdns_config/providers.tf
+++ examples/localdns_config/providers.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.3"
   required_providers {
     azapi = {
       source  = "Azure/azapi"
@@ -13,7 +14,6 @@ terraform {
       version = "~>3.0"
     }
   }
-  required_version = ">= 1.3"
 }
 
 provider "azurerm" {
